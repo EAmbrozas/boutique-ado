8ab59b39bee267fe1f0cbc56c99c524a4c9b2186
EAmbrozas <ernestasambrozas@gmail.com>
2023-07-01 21:25:45 +0000

    Add sorting JS and back to top link

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-eambrozas-boutiqueado-ph995mzss1r.ws-eu99.gitpod.io']
+ALLOWED_HOSTS = ['8000-eambrozas-boutiqueado-ph995mzss1r.ws-eu101.gitpod.io']
 
 
 # Application definition
